ad3869fce1bff3e772f0d756ad981b5d6279a933
Fabio Borges dos Santos <fabiozesge@gmail.com>
2015-10-06 15:45:51 -0300

    Adiconado padrao configuracoes

---
+++ Framework/Views/FM.UI.Configuracao.pas
@@ -0,0 +1,55 @@
+{
+Descrição: Padrão Visual do Sysmo S1 - Configuração
+}
+
+unit FM.UI.Configuracao;
+
+interface
+
+uses
+  FM.UI.Consulta, Vcl.StdCtrls, Vcl.Buttons, System.Classes,
+  Vcl.Controls, Vcl.ExtCtrls;
+
+type
+  TConfiguracaoFormView = class(TConsultaFormView)
+    BtnOk: TBitBtn;
+    BtnPadrao: TBitBtn;
+    procedure BtnOkClick(Sender: TObject);
+    procedure BtnPadraoClick(Sender: TObject);
+  end;
+
+
+
+implementation
+
+
+{$R *.dfm}
+
+
+
+procedure TPadraoVisualConfiguracao.BtnOkClick(Sender: TObject);
+begin
+//  oControlador.Executar;
+end;
+
+
+
+procedure TPadraoVisualConfiguracao.BtnPadraoClick(Sender: TObject);
+begin
+//  oControlador.UtilizarPadrao;
+end;
+
+
+
+{
+Versão do projeto: 2.17.08
+  Versão: 1.00
+  Desenvolvedor: Eliane Balbinotti
+  Data: 03/10/2012 15:40
+  Tarefa: 7767
+  Descrição: Classe base para criação de formulários.
+}
+
+
+
+end.
