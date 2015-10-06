334aa4795d08846da7b4bc8fcbec05aa8d37e2cf
Eurides Baptistella <eurides@sysmo.com.br>
2015-10-06 15:32:23 -0300

    Padrao visual cadastro

---
+++ Framework/Views/FM.UI.Cadastro.pas
@@ -0,0 +1,33 @@
+{
+Descrição: Padrão Visual do Sysmo S1 - Cadastro
+}
+
+unit FM.UI.Cadastro;
+
+interface
+
+uses
+  Vcl.Forms, Vcl.Controls, Vcl.StdCtrls, Vcl.Buttons, Classes, Vcl.ExtCtrls, FM.UI.Consulta;
+
+type
+  TCadastroFormView = class(TConsultaFormView)
+    BtnSalvar: TBitBtn;
+    BtnExcluir: TBitBtn;
+    BtnRelatorio: TBitBtn;
+    BtnEfetivar: TBitBtn;
+    BtnEditar: TBitBtn;
+  public
+    procedure BtnSalvarClick(Sender: TObject); virtual; abstract;
+    procedure BtnExcluirClick(Sender: TObject); virtual; abstract;
+    procedure BtnRelatorioClick(Sender: TObject); virtual; abstract;
+    procedure BtnEfetivarClick(Sender: TObject); virtual; abstract;
+    procedure BtnEditarClick(Sender: TObject); virtual; abstract;
+  end;
+
+implementation
+
+
+{$R *.dfm}
+
+end.
+
